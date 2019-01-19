67057e46c8975ad2df8a87c86e68557c88a7f75c
Chris McKinlay <chris.mckinlay@gmail.com>
2019-01-18 19:20:23 -0800

    Wip.

--- tensile/src/Numeric/Tensile/Tensor.hs
+++ tensile/src/Numeric/Tensile/Tensor.hs
@@ -1,31 +1,24 @@
-{-# LANGUAGE DataKinds, GADTs     #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE TypeApplications #-}
-module Numeric.Tensile.Tensor where
+module Numeric.Tensile.Tensor 
+  ( module Numeric.Tensile.Tensor,
+    module Numeric.Tensile.Tensor.Types
+  )
+where
 
-import Data.Kind (Type)
+-- TODO: reexport Types module
+
 import Data.Int (Int64)
 import GHC.TypeLits (Nat)
-
 import Numeric.Dimensions (Dimensions(..))
 import qualified Numeric.Dimensions as D
 
-
--- see https://ghc.haskell.org/trac/ghc/ticket/4009
-newtype T :: (Type -> Type) -> [Nat] -> Type -> Type where T :: f e -> T f s e deriving Show 
+import Numeric.Tensile.Tensor.Types
 
 -- | return the a runtime shape representing the dimensions of a tensor.
-shape :: forall f s e. Dimensions s => T f s e -> [Int64]
+shape :: forall s e. Dimensions s => T s e -> [Int64]
 shape _ = fromIntegral <$> D.listDims (D.dims @Nat @s)
 
 -- | Product of all dimension sizes @O(Length xs)@.
-size :: forall f s e. Dimensions s => T f s e -> Int64
+size :: forall s e. Dimensions s => T s e -> Int64
 size = product . shape
-
-
-
-
-
---
-
