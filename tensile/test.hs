081450d274b160b58f152aec2882b84ede0604de
Chris McKinlay <chris.mckinlay@gmail.com>
2019-02-06 11:35:51 -0800

    Refactor gens.

--- tensile/test.hs
+++ tensile/test.hs
@@ -2,16 +2,21 @@ import Control.Monad
 import System.Exit (exitFailure)
 import System.IO (BufferMode(..), hSetBuffering, stdout, stderr)
 
+import Hedgehog
 import qualified Hedgehog.Gen as G
+import qualified Hedgehog.Range as R
 import Numeric.Tensile.Types
 import Test.Data.Tensor.Generators
+
+rf :: Range Float
+rf = R.linearFracFrom 0 (-100) 100
 
 main :: IO ()
 main = do
   hSetBuffering stdout LineBuffering
   hSetBuffering stderr LineBuffering
 
-  s <- G.sample (tensor (dims @_ @'[2,2]) rf)
+  s <- G.sample (tensor (dims @_ @'[2,2]) G.float rf)
   print s
   results <- sequence [] :: IO [Bool]
 
