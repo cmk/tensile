2362c22d5d9a3e325a31fcb3667d583fac0014c9
Chris McKinlay <chris.mckinlay@gmail.com>
2019-02-16 17:17:17 -0800

    Add local LTS snapshot compatile with tensorflow.

--- tensile-tensorflow/lib/test/Test/Numeric/Tensile/Tensor/Gen/Internal.hs
+++ tensile-tensorflow/lib/test/Test/Numeric/Tensile/Tensor/Gen/Internal.hs
@@ -3,15 +3,11 @@ module Test.Numeric.Tensile.Tensor.Gen.Internal (gen_tensor') where
 import Numeric.Tensile.Tensor.Internal
 import Data.Vector.Storable (Vector(..),Storable(..))
 import Numeric.Tensile.Dimensions.Types (Dims(..), KnownDims(..), listDims, dims)
-import qualified Data.Vector.Storable as V
 
 import Hedgehog
 import qualified Hedgehog.Gen as G
 import qualified Hedgehog.Range as R
 
-gen_vector :: (Storable e, MonadGen m) => Range Int -> m e -> m (Vector e)
-gen_vector r g = V.fromList <$> G.list r g
 
 gen_tensor' :: (Elt e, MonadGen m) => Dims d -> m e -> m (Tensor d e)
-gen_tensor' d g = Tensor <$> gen_vector r g
-  where r = R.singleton $ fromIntegral (product $ listDims d)
+gen_tensor' d g = undefined
