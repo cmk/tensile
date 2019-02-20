06c74e0b8773feac8de0ea26d346b43fd5319782
Chris McKinlay <chris.mckinlay@gmail.com>
2019-02-19 22:38:15 -0800

    Wip prep lib for copy.

---
+++ tensile-tensorflow/ref/src/Numeric/Tensile/Operations/Linear/Internal.hs
@@ -0,0 +1,10 @@
+module Numeric.Tensile.Operations.Linear.Internal where
+
+import Numeric.Tensile.Tensor.Internal
+import Numeric.Tensile.Dimensions.Types
+import Numeric.Tensile.Dimensions.Idxs
+import Numeric.Tensile.Dimensions.Perm
+
+
+
+
---
+++ tensile-tensorflow/ref/test/Test/Numeric/Tensile/Tensor/Gen/Internal.hs
@@ -0,0 +1,11 @@
+module Test.Numeric.Tensile.Tensor.Gen.Internal where
+
+import Numeric.Tensile.Dimensions
+import Numeric.Tensile.Tensor.Internal
+import Data.Vector.Storable (Vector(..),Storable(..))
+
+import Hedgehog
+import qualified Hedgehog.Gen as G
+import qualified Hedgehog.Range as R
+
+
