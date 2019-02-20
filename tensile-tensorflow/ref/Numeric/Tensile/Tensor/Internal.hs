4d0ed9a6dcb6895688c764eb70a1cd333fe1191c
Chris McKinlay <chris.mckinlay@gmail.com>
2019-02-19 23:10:54 -0800

    Copy main lib.

---
+++ tensile-tensorflow/ref/Numeric/Tensile/Tensor/Internal.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE AllowAmbiguousTypes  #-}
+{-# LANGUAGE UndecidableInstances #-}
+
+module Numeric.Tensile.Tensor.Internal where
+
+import Control.Monad.ST (ST(..))
+import Data.Bits
+import Data.Int
+import Data.Kind
+import Data.Proxy
+import Data.Word
+import Data.Vector.Storable (Storable(..))
+import Unsafe.Coerce (unsafeCoerce)
+
+import Numeric.Tensile.Dimensions.Types
+import Numeric.Tensile.Dimensions.Idxs
+
